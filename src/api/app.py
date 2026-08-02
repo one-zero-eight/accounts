c869b72bc1481bace6f303c8468da18ac4b3ec97
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2026-08-02 22:59:01 +0300

    chore: specify cors allowed origins as regex, not as list

--- src/api/app.py
+++ src/api/app.py
@@ -34,7 +34,7 @@ patch_fastapi(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_allow_origins,
+    allow_origin_regex=settings.cors_allow_origins_regex,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
